0f287d231eb7d363bad20c09633efbee5c64a073
saurabh-m-w <smwaghmare2016@gmail.com>
2023-04-15 21:09:58 +0530

    flutter project create

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+
+const String rpcUrl ="http://127.0.0.1:7545";
+
+   // "https://rpc-mumbai.maticvigil.com/v1/a5be973518c173bacd9be16a6314dd08b6abcd23";
+
+const String contractAddress = "0x4D8158A32FF827C6eA8A275afd65803Ef08801cF";//"0xbc6d9e15255533C19f1589A7e9fbB60440a539e0";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
